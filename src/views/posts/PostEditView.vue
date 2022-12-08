95b2a43eaeb4dd97be3c7155e3103cef6e883c1d
honghao30 <58649794+honghao30@users.noreply.github.com>
2022-12-08 13:48:14 +0900

    Update PostEditView.vue

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,7 +2,23 @@
   <div class="board__wrap">
     <h2>게시판 수정</h2>
     <div class="board__wrap--innerbox">
-
+    <div class="board__wrap--innerbox">
+        <form>
+            <dl>
+              <dt>제목</dt>
+              <dd>
+                  <input type="text" title="제목" />
+              </dd>
+              <dt>내용</dt>
+              <dd>
+                  <textarea name="" id="" cols="30" rows="10"></textarea>
+              </dd>
+            </dl>
+        </form>
+        <div class="bottom__button">
+            <button type="button">목록</button>
+            <button type="button">수정</button>
+        </div>
 
 
     </div>
