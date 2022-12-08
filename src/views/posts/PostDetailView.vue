17ba36c7c7383dd44f7b00b077c93686678a636b
honghao30 <58649794+honghao30@users.noreply.github.com>
2022-12-08 13:47:21 +0900

    Update PostDetailView.vue

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -2,9 +2,18 @@
   <div class="board__wrap">
     <h2>게시판 상세보기</h2>
     <div class="board__wrap--innerbox">
-
-
-
+      <div class="board__view">
+        <h3>제목/h3>
+        <div class="context">
+            본문입니다.
+        </div>
+        <div class="creat-at">2022-12-12</div>
+      </div>
+      <div class="board-bottom">      
+        <button type="button" class="btn-blue">이전글</button>         
+        <button type="button" class="btn-blue">목록</button> 
+        <button type="button" class="btn-blue">다음글</button>      
+      </div>
     </div>
   </div>
 </template>
