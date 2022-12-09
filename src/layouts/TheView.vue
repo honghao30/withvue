28041f27a7753b54ac79ab917853fadd39549de4
honghao30 <58649794+honghao30@users.noreply.github.com>
2022-12-09 13:59:42 +0900

    Update TheView.vue

--- src/layouts/TheView.vue
+++ src/layouts/TheView.vue
@@ -1,4 +1,3 @@
-!
 <template>
   <div class="container">
     <router-view></router-view>
@@ -8,3 +7,24 @@
 <script setup></script>
 
 <style lang="scss" scoped></style>
+
+/*
+<template>
+  <div>
+    <button @click="handleClick">toggle</button>
+    <span v-if="isToggled">
+      hidden message
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let isToggled = ref(false)
+
+function handleClick() {
+  console.log('dd')  
+  isToggled.value = !isToggled.value;
+}
+</script>
+*/
